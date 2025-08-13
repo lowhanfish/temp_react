98713949d18eb2d97aba191088eba94ae12b01b6
lowhanfish <lowhanfish@gmail.com>
2025-08-13 23:01:26 +0800

    added material UI (MUI)

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Button, Typography, Container } from "@mui/material";
+
+
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container sx={{ textAlign: "center", marginTop: "50px" }}>
+      <Typography variant="h3" gutterBottom>
+        Halo Material UI 🚀
+      </Typography>
+      <Typography variant="body1">
+        Kamu sudah klik tombol ini {count} kali.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{ marginTop: 2 }}
+        onClick={() => setCount(count + 1)}
+      >
+        Klik Saya
+      </Button>
+    </Container>
   )
 }
 
