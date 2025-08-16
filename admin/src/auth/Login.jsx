71096f45e89839f3a6c18de62d51e71c2755cffb
lowhanfish <lowhanfish@gmail.com>
2025-08-16 22:36:10 +0800

    activate navigate to loggin button

--- admin/src/auth/Login.jsx
+++ admin/src/auth/Login.jsx
@@ -1,13 +1,25 @@
+import React from "react";
 import { Button, TextField, Box, Typography, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 
 export default function Login() {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        // nanti bisa ditambah validasi login di sini
+        navigate("/"); // arahkan ke Dashboard setelah login
+    };
+
+
+
     return (
         <Paper sx={{ p: 4, width: 300 }}>
             <Typography variant="h6" mb={2}>Login</Typography>
             <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
                 <TextField label="Email" fullWidth />
                 <TextField label="Password" type="password" fullWidth />
-                <Button variant="contained">Login</Button>
+                <Button onClick={handleLogin} variant="contained">Login</Button>
             </Box>
         </Paper>
     );
