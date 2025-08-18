d5cf05a0ce86f232da9556e36c24924997930039
lowhanfish <lowhanfish@gmail.com>
2025-08-18 15:44:18 +0800

    added Autocomplete Styling

--- admin/src/assets/styling/style.js
+++ admin/src/assets/styling/style.js
@@ -1,5 +1,8 @@
-import { styled } from "@mui/material/styles";
+import { styled, alpha } from "@mui/material/styles";
 import ListItem from "@mui/material/ListItem";
+import InputBase from "@mui/material/InputBase";
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 // Custom ListItem untuk sidebar
 const ListItemSatu = styled(ListItem)(({ theme }) => ({
@@ -11,6 +14,66 @@ const ListItemSatu = styled(ListItem)(({ theme }) => ({
   "&:hover": {
     backgroundColor: theme.palette.action.hover,
   },
+}));
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  'label + &': {
+    marginTop: theme.spacing(3),
+  },
+  '& .MuiInputBase-input': {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: '#F3F6F9',
+    border: '1px solid',
+    borderColor: '#E0E3E7',
+    fontSize: 16,
+    width: 'auto',
+    padding: '10px 12px',
+    transition: theme.transitions.create([
+      'border-color',
+      'background-color',
+      'box-shadow',
+    ]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+    ...theme.applyStyles('dark', {
+      backgroundColor: '#1A2027',
+      borderColor: '#2D3843',
+    }),
+  },
+}));
+
+const Fieldx = styled(TextField)(({theme})=>({
+  backgroundColor :'#fff',
+  borderRadius : 4,
+  "& .MuiInputBase-input": {
+    fontSize: "13px",   // ukuran teks input
+    padding: "8px 10px" // padding dalam input
+  }
+}));
+const Autocompletex = styled(Autocomplete)(({theme})=>({
+  width: '100%',
+    background: 'white',
+    borderRadius: 4,
+    "& .MuiInputBase-input": {
+      fontSize: "13px",   // ukuran teks input
+      padding: "6px 8px"  // padding biar lebih rapat
+    }
 }));
 
 
@@ -35,4 +98,4 @@ const stylex = {
   },
 }
 
-export { ListItemSatu, stylex };
+export { ListItemSatu, BootstrapInput, Fieldx, Autocompletex, stylex };
--- admin/src/pages/example/template1.jsx
+++ admin/src/pages/example/template1.jsx
@@ -1,15 +1,71 @@
-import { useState } from "react";
+import * as React from 'react';
+// import { alpha, styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import InputBase from '@mui/material/InputBase';
+import { stylex, BootstrapInput, Fieldx, Autocompletex } from '../../assets/styling/style';
+
 
 
 const Template1 = () => {
 
+
+
+    const top100Films = [
+        { id: 1, label: 'The Shawshank Redemption', year: 1994 },
+        { id: 2, label: 'The Godfather', year: 1972 },
+        { id: 3, label: 'The Godfather: Part II', year: 1974 },
+        { id: 4, label: 'The Dark Knight', year: 2008 },
+        { id: 5, label: '12 Angry Men', year: 1957 },
+        { id: 6, label: "Schindler's List", year: 1993 },
+    ];
+
+    const [value, setValue] = React.useState(top100Films[0]);
+    const [inputValue, setInputValue] = React.useState('');
+
     return (
         <div className="cardx">
             <div className="cardxHeader">
-                <div>Yah</div>
+                <Grid container spacing={1}>
+                    <Grid size={{ md: 4, xs: 12 }}>
+                        <Fieldx size='small' fullWidth id="outlined-basic" variant="outlined" />
+                    </Grid>
+                    <Grid size={{ md: 4, xs: 12 }}>
+                        <Fieldx size='small' fullWidth id="outlined-basic" variant="outlined" />
+                    </Grid>
+                    <Grid size={{ md: 4, xs: 12 }}>
+                        <Autocompletex
+                            value={value}
+                            onChange={(event, newValue) => {
+                                setValue(newValue);
+                            }}
+                            inputValue={inputValue}
+                            onInputChange={(event, newInputValue) => {
+                                setInputValue(newInputValue);
+                            }}
+                            size="small"
+                            options={top100Films}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+
+                        {/* <div>{`value: ${value !== null ? `'${value.id}'` : 'null'}`}</div>
+                        <div>{`inputValue: '${inputValue}'`}</div>
+                        <br /> */}
+
+                    </Grid>
+                </Grid>
             </div>
             <div className="cardxBody">
-                <div class="table-wrap" tabindex="0">
+
+
+
+
+                <div className="table-wrap" tabIndex="0">
                     <table className="tabelku">
                         <thead className="h_thead">
                             <tr>
@@ -24,31 +80,31 @@ const Template1 = () => {
                                 <td>Rina Saputri</td>
                                 <td>rina@example.com</td>
                                 <td>Bandung</td>
-                                <td><span class="badge ok">Aktif</span></td>
+                                <td><span className="badge ok">Aktif</span></td>
                             </tr>
                             <tr>
                                 <td>Galang Aditya</td>
                                 <td>galang@example.com</td>
                                 <td>Jakarta</td>
-                                <td><span class="badge warn">Menunggu</span></td>
+                                <td><span className="badge warn">Menunggu</span></td>
                             </tr>
                             <tr>
                                 <td>Devi Maharani</td>
                                 <td>devi@example.com</td>
                                 <td>Makassar</td>
-                                <td><span class="badge ok">Aktif</span></td>
+                                <td><span className="badge ok">Aktif</span></td>
                             </tr>
                             <tr>
                                 <td>Rafi Pratama</td>
                                 <td>rafi@example.com</td>
                                 <td>Yogyakarta</td>
-                                <td><span class="badge ok">Aktif</span></td>
+                                <td><span className="badge ok">Aktif</span></td>
                             </tr>
                             <tr>
                                 <td>Nadia Lestari</td>
                                 <td>nadia@example.com</td>
                                 <td>Surabaya</td>
-                                <td><span class="badge warn">Menunggu</span></td>
+                                <td><span className="badge warn">Menunggu</span></td>
                             </tr>
                         </tbody>
                     </table>
