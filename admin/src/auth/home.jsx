cf16e3f82272cf59f0ef21bfee468a6cdfb910db
lowhanfish <lowhanfish@gmail.com>
2025-08-25 23:52:14 +0800

    modify Home component

--- admin/src/auth/home.jsx
+++ admin/src/auth/home.jsx
@@ -7,33 +7,30 @@ import Grid from '@mui/material/Grid';
 function Home() {
     return (
         <div className='singlePage'>
-            {/* <Grid container spacing={1}>
-                <Grid size={{ md: 6, sx: 12 }}>
-                    <h1>Test</h1>
-                </Grid>
-                <Grid size={{ md: 6, sx: 12 }}>
-                    <h1>Test</h1>
-                </Grid>
-
-            </Grid> */}
-
             <Grid container spacing={1}>
-                <Grid size={{ md: 4, xs: 12 }}>
-                    {/* <FieldWithButton placeholderx={'Cari Data..'} /> */}
-                    1
+                <Grid size={{ md: 6, xs: 12 }}>
+                    <div className='singlePageContainer'></div>
                 </Grid>
-                <Grid size={{ md: 4, xs: 12 }}>
-                    2
-                    {/* <FieldSingle /> */}
+                <Grid size={{ md: 6, xs: 12 }}>
+                    <div className='singlePageContainer' style={{ paddingLeft: '10%', paddingRight: '10%' }}>
+                        <div className='singlePageTextContainer'>
+                            <div className='h_Home1'>Welcome to</div>
+                            <div className='h_Home5'>Temp-React</div>
+                            <div style={{ marginTop: 2 }} className='h_Home2'>This is an admin version template built with ReactJS. This template is a frontend that can be used as a basis for developing your application.</div>
+                            <div style={{ marginTop: 15 }} className='h_Home3'>
+                                You can also contribute to the development of this application on the page
+                            </div>
+                            <div className='h_Home4'>
+                                <a className='h_Home4' href="https://github.com/lowhanfish/temp_react">https://github.com/lowhanfish/temp_react</a>
+                            </div>
+
+                        </div>
+                    </div>
                 </Grid>
-                <Grid size={{ md: 4, xs: 12 }}>
-                    3
 
-                    {/* <FieldAutocomplete /> */}
-
-
-                </Grid>
             </Grid>
+
+
         </div>
     )
 }
--- admin/src/layouts/AuthLayout.jsx
+++ admin/src/layouts/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 
 export default function AuthLayout() {
     return (
-        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", flex: 1, marginLeft: -1, marginRight: -1, marginTop: -1 }}>
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", flex: 1, margin: -1 }}>
             <Outlet />
         </Box>
         // <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
